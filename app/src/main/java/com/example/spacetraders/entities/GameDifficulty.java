b44b85d9134d370808ef73b18ec85d337c30b8ea
Ryan Soedjak <soedjak.ryan@gmail.com>
2019-02-15 16:13:02 -0500

    reorganization

---
+++ app/src/main/java/com/example/spacetraders/entities/GameDifficulty.java
@@ -0,0 +1,8 @@
+package com.example.spacetraders.entity;
+
+/**
+ * This class represents the various game difficulties
+ */
+public enum GameDifficulty {
+    EASY, NORMAL, HARD;
+}
--- app/src/main/java/com/example/spacetraders/views/MainActivity.java
+++ app/src/main/java/com/example/spacetraders/views/MainActivity.java
@@ -1,4 +1,4 @@
-package com.example.spacetraders;
+package com.example.spacetraders.views;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
