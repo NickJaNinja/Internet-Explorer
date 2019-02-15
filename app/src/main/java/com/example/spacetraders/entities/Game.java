0c9f7916d107153223208088d54389270f44cecf
Cecilia <jliu738@gatech.edu>
2019-02-15 17:48:10 -0500

    game.java

--- app/src/main/java/com/example/spacetraders/entities/Game.java
+++ app/src/main/java/com/example/spacetraders/entities/Game.java
@@ -10,17 +10,10 @@ public class Game {
     private Player player;
 
     /**
-<<<<<<< HEAD
-     * constructor for game
-     *
-     * @param difficulty difficulty selected
-     * @param player player in the game
-=======
      * Constructor for the game
      *
      * @param difficulty   difficulty of game
      * @param player        the player
->>>>>>> 1c5d8f30f36a03b6b2dd13e61a8e52860dfd4359
      */
     public Game(GameDifficulty difficulty, Player player) {
         this.difficulty = difficulty;
