0c264033b44868c3ace5596a7cb00f5329526ff1
Cecilia <jliu738@gatech.edu>
2019-02-15 17:21:34 -0500

    player.java

--- app/src/main/java/com/example/spacetraders/entities/Player.java
+++ app/src/main/java/com/example/spacetraders/entities/Player.java
@@ -2,6 +2,9 @@ package com.example.spacetraders.entity;
 
 /**
  * This class represents the player
+ *
+ * @author Internet-Explorer
+ * @version 1.0
  */
 public class Player {
     private String name;
@@ -12,10 +15,17 @@ public class Player {
     private int credits;
     private Spaceship ship;
 
-    public Player(String name, int pilotSkill, int fighterSkill, int traderSkill, int engineerSkill) {
-        this(name, pilotSkill, fighterSkill, traderSkill, engineerSkill, 1000, Spaceship.GNAT);
-    }
-
+    /**
+     * constuctor with all parameters
+     *
+     * @param name player's name
+     * @param pilotSkill initial pilotSkill
+     * @param fighterSkill initial fighterSkill
+     * @param traderSkill initial traderSkill
+     * @param engineerSkill inital engineerSkill
+     * @param credits initial credits
+     * @param ship initial ship
+     */
     public Player(String name, int pilotSkill, int fighterSkill, int traderSkill, int engineerSkill, int credits, Spaceship ship) {
         this.name = name;
         this.pilotSkill = pilotSkill;
@@ -26,4 +36,142 @@ public class Player {
         this.ship = ship;
     }
 
+    /**
+     * constuctor with default values for credit = 1000 & ship = GNAT
+     *
+     * @param name player's name
+     * @param pilotSkill initial pilotSkill
+     * @param fighterSkill initial fighterSkill
+     * @param traderSkill initial traderSkill
+     * @param engineerSkill inital engineerSkill
+     */
+    public Player(String name, int pilotSkill, int fighterSkill, int traderSkill, int engineerSkill) {
+        this(name, pilotSkill, fighterSkill, traderSkill, engineerSkill, 1000, Spaceship.GNAT);
+    }
+
+    /**
+     * getter for name
+     *
+     * @return name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * getter for pilotSkill
+     *
+     * @return pilotSkill
+     */
+    public int getPilotSkill() {
+        return pilotSkill;
+    }
+
+    /**
+     * getter for fighterSkill
+     *
+     * @return fighterSkill
+     */
+    public int getFighterSkill() {
+        return fighterSkill;
+    }
+
+    /**
+     * getter for traderSkill
+     *
+     * @return traderSkill
+     */
+    public int getTraderSkill() {
+        return traderSkill;
+    }
+
+    /**
+     * getter for engineerSkill
+     *
+     * @return engineerSkill
+     */
+    public int getEngineerSkill() {
+        return engineerSkill;
+    }
+
+    /**
+     * getter for credits
+     *
+     * @return credits
+     */
+    public int getCredits() {
+        return credits;
+    }
+
+    /**
+     * getter for ship
+     *
+     * @return ship
+     */
+    public Spaceship getShip() {
+        return ship;
+    }
+
+    /**
+     * setter for name
+     *
+     * @param name player's name
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * setter for pilotSkill
+     *
+     * @param pilotSkill player's pilotSkill
+     */
+    public void setPilotSkill(int pilotSkill) {
+        this.pilotSkill = pilotSkill;
+    }
+
+    /**
+     * setter for fighterSkill
+     *
+     * @param fighterSkill player's fighterSkill
+     */
+    public void setFighterSkill(int fighterSkill) {
+        this.fighterSkill = fighterSkill;
+    }
+
+    /**
+     * setter for traderSkill
+     *
+     * @param traderSkill player's traderSkill
+     */
+    public void setTraderSkill(int traderSkill) {
+        this.traderSkill = traderSkill;
+    }
+
+    /**
+     * setter for engineerSkill
+     *
+     * @param engineerSkill player's engineerSkill
+     */
+    public void setEngineerSkill(int engineerSkill) {
+        this.engineerSkill = engineerSkill;
+    }
+
+    /**
+     * setter for credits
+     *
+     * @param credits player's credits
+     */
+    public void setCredits(int credits) {
+        this.credits = credits;
+    }
+
+    /**
+     * setter for ship
+     *
+     * @param ship player's ship
+     */
+    public void setShip(Spaceship ship) {
+        this.ship = ship;
+    }
 }
