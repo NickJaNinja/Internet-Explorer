699a0fa83ccb94e6d02b345b28be47d2b76d018a
Cecilia <jliu738@gatech.edu>
2019-02-15 17:45:17 -0500

    game.java

--- app/src/main/java/com/example/spacetraders/entities/Game.java
+++ app/src/main/java/com/example/spacetraders/entities/Game.java
@@ -7,12 +7,27 @@ public class Game {
     private GameDifficulty difficulty;
     private Player player;
 
+    /**
+     * constructor for game
+     *
+     * @param difficulty difficulty selected
+     * @param player player in the game
+     */
     public Game(GameDifficulty difficulty, Player player) {
         this.difficulty = difficulty;
         this.player = player;
     }
 
+    /**
+     * getter for game difficulty
+     *
+     * @return difficulty
+     */
     public GameDifficulty getGameDifficulty() {return difficulty;}
 
+    /**
+     * getter for player
+     * @return player
+     */
     public Player getPlayer() {return player;}
 }
