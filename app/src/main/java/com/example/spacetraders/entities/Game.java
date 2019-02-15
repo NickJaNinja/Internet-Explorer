1c5d8f30f36a03b6b2dd13e61a8e52860dfd4359
Ryan Soedjak <soedjak.ryan@gmail.com>
2019-02-15 17:42:14 -0500

    javadocs

--- app/src/main/java/com/example/spacetraders/entities/Game.java
+++ app/src/main/java/com/example/spacetraders/entities/Game.java
@@ -4,9 +4,17 @@ package com.example.spacetraders.entities;
  * This class represents the game
  */
 public class Game {
+    /** the game difficulty */
     private GameDifficulty difficulty;
+    /** the player */
     private Player player;
 
+    /**
+     * Constructor for the game
+     *
+     * @param difficulty   difficulty of game
+     * @param player        the player
+     */
     public Game(GameDifficulty difficulty, Player player) {
         this.difficulty = difficulty;
         this.player = player;
