666221d751f1e8bef4ed3fd035956240afcb30b2
Ryan Soedjak <soedjak.ryan@gmail.com>
2019-02-15 16:19:28 -0500

    add empty folders

---
+++ app/src/main/java/com/example/spacetraders/entities/Player.java
@@ -0,0 +1,16 @@
+package com.example.spacetraders.entity;
+
+/**
+ * This class represents the player
+ */
+public class Player {
+    private string name;
+    private int pilotSkill;
+    private int fighterSkill;
+    private int traderSkill;
+    private int engineerSkill;
+
+
+    Player() {}
+    
+}
