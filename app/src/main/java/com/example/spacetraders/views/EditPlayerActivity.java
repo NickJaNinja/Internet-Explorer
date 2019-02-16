439ca677dad59349000b5f4d79b8a24634f033d1
NickJaNinja <ndf99551@uga.edu>
2019-02-15 19:08:19 -0500

    trying to add onCreate functionality

--- app/src/main/java/com/example/spacetraders/views/EditPlayerActivity.java
+++ app/src/main/java/com/example/spacetraders/views/EditPlayerActivity.java
@@ -3,15 +3,23 @@ package com.example.spacetraders.views;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
+
+import com.example.spacetraders.entities.Game;
 import com.example.spacetraders.entities.GameDifficulty;
 import com.example.spacetraders.R;
+import com.example.spacetraders.entities.Player;
+import com.example.spacetraders.viewmodels.EditPlayerViewModel;
 
 public class EditPlayerActivity extends AppCompatActivity {
+    private EditPlayerViewModel editPlayerViewModel;
+
     private EditText nameField;
     private Button fightPlus;
     private Button fightMinus;
@@ -29,6 +37,7 @@ public class EditPlayerActivity extends AppCompatActivity {
     private TextView pilotText;
     private TextView difficultyText;
 
+    private Game g;
     private Player p;
 
     private boolean editing;
@@ -36,7 +45,7 @@ public class EditPlayerActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_edit_player);
+        setContentView(R.layout.config);
 
         // I HAVE NO IDEA WHAT THIS DOES
         if (getSupportActionBar() != null) {
@@ -62,10 +71,25 @@ public class EditPlayerActivity extends AppCompatActivity {
         difficultyText = findViewById();
         Button button = findViewById(R.id.ok_button);
 
-        ArrayAdapter<String> difficultyAdapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, GameDifficulty.values());
+        ArrayAdapter<GameDifficulty> difficultyAdapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, GameDifficulty.values());
         difficultyAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         gameDifficultySpinner.setAdapter(difficultyAdapter);
 
+        p = getIntent().getSerializableExtra()
         gameDifficultySpinner.setSelection();
+    }
+
+    public void onOkPressed(View view) {
+        Log.d("Edit", "OK Player Pressed");
+
+        p.setName(nameField.getText().toString());
+        p.setEngineerSkill(Integer.parseInt((String)engineerText.getText()));
+        p.setFighterSkill(Integer.parseInt((String)fighterText.getText()));
+        p.setTraderSkill(Integer.parseInt((String)tradeText.getText()));
+        p.setPilotSkill(Integer.parseInt((String)pilotText.getText()));
+
+        GameDifficulty diff = (GameDifficulty) gameDifficultySpinner.getSelectedItem();
+
+        g = new Game(diff, p);
     }
 }
