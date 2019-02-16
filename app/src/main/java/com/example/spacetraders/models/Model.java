76e96fccd2c5e19f102d95724c7f388302faabd5
NickJaNinja <ndf99551@uga.edu>
2019-02-16 15:16:44 -0500

    redoing MVVM files

---
+++ app/src/main/java/com/example/spacetraders/models/Model.java
@@ -0,0 +1,12 @@
+package com.example.spacetraders.models;
+
+import com.example.spacetraders.entities.Game;
+
+public class Model {
+    private Game g;
+
+
+    public void createGame() {
+
+    }
+}
