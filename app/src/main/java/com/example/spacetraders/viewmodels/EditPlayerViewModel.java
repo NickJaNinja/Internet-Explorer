9f71a04c4951c8e4013fa6acf8d46adec4fa7d7b
NickJaNinja <ndf99551@uga.edu>
2019-02-16 15:46:00 -0500

    working on the viewmodel passing data and nonsense

--- app/src/main/java/com/example/spacetraders/viewmodels/EditPlayerViewModel.java
+++ app/src/main/java/com/example/spacetraders/viewmodels/EditPlayerViewModel.java
@@ -2,8 +2,28 @@ package com.example.spacetraders.viewmodels;
 
 import android.app.Application;
 import android.arch.lifecycle.AndroidViewModel;
+import android.os.Bundle;
 import android.support.annotation.NonNull;
 
 public class EditPlayerViewModel extends AndroidViewModel {
     public EditPlayerViewModel(@NonNull Application app) { super(app); }
+
+    /**
+     *
+     *
+     * @param name
+     * @param fight
+     * @param trade
+     * @param eng
+     * @param pilot
+     * @return
+     */
+    public boolean onOk(String name, int fight, int trade, int eng, int pilot) {
+        if (name == null || name.length() < 1) { return false; }
+        int sum = fight + trade + eng + pilot;
+        if (sum != 16) {
+            return false;
+        }
+        return true;
+    }
 }
