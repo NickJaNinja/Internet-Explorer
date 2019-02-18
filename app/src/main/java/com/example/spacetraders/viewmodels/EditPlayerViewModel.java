bb0518aadbd7b04371d034c8f15245c69bada28f
NickJaNinja <ndf99551@uga.edu>
2019-02-18 13:55:02 -0500

    removed unused import

--- app/src/main/java/com/example/spacetraders/viewmodels/EditPlayerViewModel.java
+++ app/src/main/java/com/example/spacetraders/viewmodels/EditPlayerViewModel.java
@@ -3,7 +3,6 @@ package com.example.spacetraders.viewmodels;
 import android.app.Application;
 import android.arch.lifecycle.AndroidViewModel;
 import android.graphics.Color;
-import android.os.Bundle;
 import android.support.annotation.NonNull;
 
 public class EditPlayerViewModel extends AndroidViewModel {
