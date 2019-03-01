077c7fa810def5a84f19a32cc1ac44fec2c16fbc
NickJaNinja <ndf99551@uga.edu>
2019-03-01 18:45:05 -0500

    set up planetbuttons

--- app/src/main/java/com/example/spacetraders/views/PlanetActivity.java
+++ app/src/main/java/com/example/spacetraders/views/PlanetActivity.java
@@ -1,20 +1,25 @@
 package com.example.spacetraders.views;
 
 import android.os.Bundle;
-import android.widget.Button;
+import android.widget.TextView;
 
 import com.example.spacetraders.R;
 
 public class PlanetActivity extends GUIActivity {
-    private Button market;
-    private Button upgrade;
-    private Button refuel;
-    private Button leaveOrbit;
+    private TextView market;
+    private TextView upgrade;
+    private TextView refuel;
+    private TextView leaveOrbit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.planet);
+
+        market = findViewById(R.id.market_text);
+        upgrade = findViewById(R.id.upgrade_text);
+        refuel = findViewById(R.id.refuel_text);
+        leaveOrbit = findViewById(R.id.leave_orbit__text);
     }
 
 }
