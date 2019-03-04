2a5d2c1487929ad36d2c29be43bf5d4ba6da3803
dbump3 <dbump3@gatech.edu>
2019-03-04 11:59:32 -0500

    some work on market.xml

--- app/src/main/java/com/example/spacetraders/views/ShopActivity.java
+++ app/src/main/java/com/example/spacetraders/views/ShopActivity.java
@@ -1,12 +1,19 @@
 package com.example.spacetraders.views;
 
 import android.os.Bundle;
+import android.widget.Button;
+
+import com.example.spacetraders.R;
 
 public class ShopActivity extends GUIActivity {
+    private Button confirm;
+    private Button cancel;
+    private Button leaveMarket;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.market);
     }
 
 }
