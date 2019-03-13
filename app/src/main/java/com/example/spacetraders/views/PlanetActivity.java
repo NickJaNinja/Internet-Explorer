b98c823d24e1b8ad8daebd33f382be9413833710
NickJaNinja <ndf99551@uga.edu>
2019-03-12 20:46:17 -0400

    tryin stuff with the action bar

--- app/src/main/java/com/example/spacetraders/views/PlanetActivity.java
+++ app/src/main/java/com/example/spacetraders/views/PlanetActivity.java
@@ -2,6 +2,7 @@ package com.example.spacetraders.views;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -34,4 +35,7 @@ public class PlanetActivity extends GUIActivity {
             }
         });
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) { return true; }
 }
