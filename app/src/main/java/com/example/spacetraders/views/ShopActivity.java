60861ad01795cd5750d8e852385ef8aeb1c3214a
dbump3 <dbump3@gatech.edu>
2019-03-13 23:51:10 -0400

    reeeee

--- app/src/main/java/com/example/spacetraders/views/ShopActivity.java
+++ app/src/main/java/com/example/spacetraders/views/ShopActivity.java
@@ -56,7 +56,6 @@ public class ShopActivity extends GUIActivity {
         recyclerViewPlayer.setAdapter(adapterForPlayer);
 
 
-
         //buyList = findViewById(R.id.buy_list);
         //for (int c = 0; c < adapter.getItemCount(); c++) {
             //buyList.addView(adapter.getLayout(c, this));
