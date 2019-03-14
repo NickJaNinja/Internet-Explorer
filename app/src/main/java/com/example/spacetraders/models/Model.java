e0e641513a2302eceab373ab7641899017278742
Ryan Soedjak <soedjak.ryan@gmail.com>
2019-03-14 01:29:09 -0400

    format

--- app/src/main/java/com/example/spacetraders/models/Model.java
+++ app/src/main/java/com/example/spacetraders/models/Model.java
@@ -16,24 +16,28 @@ public class Model {
     private Shop currShop;
     private ShopEntry entryToSwap;
 
-    /** Singleton Pattern Code
-     *  this allows us to get access to this class
-     *  anywhere, which will allow our View models to access
-     *  the "back end"  more easily
+    /**
+     * Singleton Pattern Code
+     * this allows us to get access to this class
+     * anywhere, which will allow our View models to access
+     * the "back end"  more easily
      */
-    private static  Model instance = new Model();
+    private static Model instance = new Model();
+
     /**
      * getter for instance
      *
      * @return instance
      */
-    public static Model getInstance() { return instance; }
+    public static Model getInstance() {
+        return instance;
+    }
 
     /**
      * Creates a new game
      *
      * @param gd game difficulty
-     * @param p player
+     * @param p  player
      */
     public void createGame(GameDifficulty gd, Player p) {
         game = new Game(gd, p);
@@ -43,9 +47,9 @@ public class Model {
      * Passes a good, an amount, and a price all the way down to player
      * and ship to be able to carry out a transaction if valid
      *
-     * @param sg the type of the good associated with this transaction
+     * @param sg     the type of the good associated with this transaction
      * @param amount the total amount of the good to buy
-     * @param price the price of each good
+     * @param price  the price of each good
      * @return 1 if transaction occurred, 0 otherwise
      */
     public int makeTransaction(ShopGoods sg, int amount, int price) {
@@ -69,36 +73,52 @@ public class Model {
     public List<ShopEntry> getPlayerEntries(Ship ship) {
         return ship.getInventoryCargo();
     }
+
     /**
      * getter for game
      *
      * @return game
      */
-    public Game getGame() { return game; }
+    public Game getGame() {
+        return game;
+    }
+
     /**
      * getter for current planet
      *
      * @return current planet
      */
-    public Planet getCurrentPlanet() { return game.getCurrentPlanet(); }
+    public Planet getCurrentPlanet() {
+        return game.getCurrentPlanet();
+    }
+
     /**
      * getter for shop
      *
      * @return shop
      */
-    public Shop getShop() { return currShop; }
+    public Shop getShop() {
+        return currShop;
+    }
+
     /**
      * setter for shop
+     *
      * @param s the new shop
      */
-    public void setShop(Shop s) { currShop = s; }
+    public void setShop(Shop s) {
+        currShop = s;
+    }
+
     /**
      * setter for entry to swap
+     *
      * @param entryToSwap the entry to swap
      */
     public void setEntryToSwap(ShopEntry entryToSwap) {
         this.entryToSwap = entryToSwap;
     }
+
     /**
      * getter for entry to swap
      *
