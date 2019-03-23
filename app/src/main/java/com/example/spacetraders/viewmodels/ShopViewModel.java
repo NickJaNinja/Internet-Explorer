5e688caac9d7be29af17fac772017469377f4d62
Cecilia <jliu738@gatech.edu>
2019-03-23 11:46:05 -0400

    map

--- app/src/main/java/com/example/spacetraders/viewmodels/ShopViewModel.java
+++ app/src/main/java/com/example/spacetraders/viewmodels/ShopViewModel.java
@@ -16,42 +16,11 @@ public class ShopViewModel extends AndroidViewModel {
     private Model model;
     private List<ShopEntry> shopInventoryTemp;
     private List<ShopEntry> playerInventoryTemp;
-    private Shop shop;
-
+    
     public ShopViewModel(@NonNull Application app) {
         super(app);
         model = Model.getInstance();
     }
-
-//    /***
-//     * check if the player have enough money and cargo hold for buying the good
-//     * @param price, the price of a good
-//     * @param creditRemaining, the player's remaining money
-//     * @param cargoHoldRemaining, the remaining cargoHolds in the ship
-//     * @return true if purchase is allowed
-//     */
-//    public boolean onBuy(int price, int creditRemaining, int cargoHoldRemaining) {
-//        if (price > creditRemaining || cargoHoldRemaining <= 0) {
-//            return false;
-//        }
-//        return true;
-//    }
-//
-//    /**
-//     * check if the selected good to sell is in play's cargo
-//     *
-//     * @param good,        the good to be sold
-//     * @param goodsInCargo list of all goods in play's cargo
-//     * @return true if cargo contains the good to be sold, transaction is allowed
-//     */
-//    public boolean onSell(ShopGoods good, List<ShopGoods> goodsInCargo) {
-//        for (int i = 0; i < goodsInCargo.size(); i++) {
-//            if (good == goodsInCargo.get(i)) {
-//                return true;
-//            }
-//        }
-//        return false;
-//    }
 
     /**
      * initializes market
