b12bde6b0ab102a403266c9c063391220ea1e20b
Cecilia <jliu738@gatech.edu>
2019-03-22 20:39:21 -0400

    map

--- app/src/main/java/com/example/spacetraders/entities/Universe.java
+++ app/src/main/java/com/example/spacetraders/entities/Universe.java
@@ -135,11 +135,11 @@ public class Universe {
     /**
      * dimensions of universe
      */
-    private static final int MAXX = 2000;
-    private static final int MAXY = 2000;
+    private static final int MAXX = 100;
+    private static final int MAXY = 100;
 
     private SolarSystem[] solarSystems;
-    private Object[][] mapOfSolarSystem;
+    private SolarSystem[][] mapOfSolarSystem;
 
     private Random r = new Random();
 
@@ -148,7 +148,7 @@ public class Universe {
      */
     public Universe() {
         Set<Coordinates> coordSet = new HashSet<Coordinates>();
-        mapOfSolarSystem = new Object[2000][2000];
+        mapOfSolarSystem = new SolarSystem[100][100];
         this.solarSystems = new SolarSystem[SOLARSYSTEMNAMES.length];
         for (int i = 0; i < SOLARSYSTEMNAMES.length; i++) {
             //keep making random coordinates until we find one that hasn't been used yet
