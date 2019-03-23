8b98a15d9caf95279938d7502c5b4e968ebfed1f
yahuann <yahuanzhu@gmail.com>
2019-03-23 19:26:54 -0400

    solarsystemactivity

--- app/src/main/java/com/example/spacetraders/views/SolarSystemActivity.java
+++ app/src/main/java/com/example/spacetraders/views/SolarSystemActivity.java
@@ -1,12 +1,26 @@
 package com.example.spacetraders.views;
 
+import android.arch.lifecycle.ViewModelProviders;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.FrameLayout;
+import android.widget.TextView;
 
 import com.example.spacetraders.R;
+import com.example.spacetraders.entities.SolarSystem;
+import com.example.spacetraders.models.Model;
+import com.example.spacetraders.viewmodels.SolarSystemViewModel;
 
 public class SolarSystemActivity extends GUIActivity {
+    private TextView name;
+    private TextView distance;
+    private TextView coordinates;
+    private RecyclerView recyclerViewPlanet;
+    private Model model;
+    private SolarSystemViewModel viewModel;
+    private PlanetAdapter adapterForPlanets;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,5 +31,23 @@ public class SolarSystemActivity extends GUIActivity {
         View.inflate(this, R.layout.menu_bar, layout);
 
         setContentView(layout);
+
+        this.model = Model.getInstance();
+        viewModel = ViewModelProviders.of(this).get(SolarSystemViewModel.class);
+
+        recyclerViewPlanet = findViewById(R.id.buy_list);
+        recyclerViewPlanet.setLayoutManager(new LinearLayoutManager(this));
+        recyclerViewPlanet.setHasFixedSize(true);
+
+        adapterForPlanets = new PlanetAdapter(viewModel.getPlanets());
+        recyclerViewPlanet.setAdapter(adapterForPlanets);
+
+
+
+
+
+
+
+
     }
 }
