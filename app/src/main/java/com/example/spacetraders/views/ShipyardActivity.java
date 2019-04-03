2534aa4d22be75d9d0841ee23333a4d93e5dd35d
dbump3 <dbump3@gatech.edu>
2019-04-02 21:54:12 -0400

    what the hell did you do nick

--- app/src/main/java/com/example/spacetraders/views/ShipyardActivity.java
+++ app/src/main/java/com/example/spacetraders/views/ShipyardActivity.java
@@ -1,4 +1,17 @@
 package com.example.spacetraders.views;
 
-public class ShipyardActivity {
+import android.os.Bundle;
+
+import com.example.spacetraders.R;
+
+public class ShipyardActivity extends GUIActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.shipyard);
+
+
+    }
 }
