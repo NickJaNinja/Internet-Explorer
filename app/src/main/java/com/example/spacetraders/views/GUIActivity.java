5afcef7797326dc31cc2e705c92b7a608afeb487
Cecilia <jliu738@gatech.edu>
2019-04-02 21:21:15 -0400

    HELLO

--- app/src/main/java/com/example/spacetraders/views/GUIActivity.java
+++ app/src/main/java/com/example/spacetraders/views/GUIActivity.java
@@ -4,10 +4,18 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.Button;
 
+/**
+ * GUI activity class
+ */
 public class GUIActivity extends AppCompatActivity {
     private Button inventory;
     private Button status;
 
+    /**
+     * on create
+     *
+     * @param savedInstanceState bundle of saved instance state
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
