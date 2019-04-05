d60ee66be4b983e87b20cb5780a36c57bedc7958
Ryan Soedjak <soedjak.ryan@gmail.com>
2019-04-04 22:40:28 -0400

    removed unused import statements

--- app/src/main/java/com/example/spacetraders/entities/StarClass.java
+++ app/src/main/java/com/example/spacetraders/entities/StarClass.java
@@ -2,15 +2,13 @@ package com.example.spacetraders.entities;
 
 import android.graphics.Color;
 
-import java.util.Random;
-
 public enum StarClass {
     O('O', 0.004f, Color.parseColor("#9BB0FF"), 30000, 99999, 6.6,  99.99),
     B('B', 0.027f, Color.parseColor("#BBCCFF"), 10000, 30000, 1.8,  6.6),
     A('A', 0.10f,  Color.parseColor("#FBF8FF"), 7500,  10000, 1.4,  1.8),
     F('F', 0.24f,  Color.parseColor("#FFFFED"), 6000,  7500,  1.15, 1.4),
     G('G', 0.35f,  Color.parseColor("#FFFF00"), 5200,  6000,  0.96, 1.15),
-    K('K', 0.60f,  Color.parseColor("#FF9833"), 3700,  5200,  0.7,  0.96),
+    K('K', 0.60f,  Color.parseColor(s"#FF9833"), 3700,  5200,  0.7,  0.96),
     M('M', 1.00f,  Color.parseColor("#FF0000"), 2400,  3700,  0.12, 0.7);
 
     private final char classification;
@@ -21,7 +19,7 @@ public enum StarClass {
     private final double radiusLower;
     private final double radiusUpper;
 
-    private final Random r = new Random();
+    //private final Random r = new Random();
 
     StarClass(char classification, float chance, int color, int tempLower,
               int tempUpper, double radiusLower, double radiusUpper) {
