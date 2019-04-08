c23089fee0b91bd3017c98382c29a8c8dd188566
NickJaNinja <ndf99551@uga.edu>
2019-04-08 16:39:27 -0400

    test

---
+++ app/src/androidTest/java/com/example/spacetraders/RestockTest.java
@@ -0,0 +1,20 @@
+package com.example.spacetraders;
+
+import org.junit.Test;
+
+/**
+ * Restock test
+**/
+
+class RestockTest {
+
+    @Test
+    public void doTest() {
+        
+    }
+
+    public static void main(String args[]) {
+       System.out.println("Test.");
+    }
+}
+
